c87b183da63dd735035280073696579088c7350c
esrafrb <esrafrb@gmail.com>
2026-06-25 14:05:37 +0300

    iOS Crash Fix: Remove manual Firebase initialization from AppDelegate since flutter firebase_core handles it

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,8 +1,5 @@
 import UIKit
 import Flutter
-import Firebase
-import FirebaseMessaging
-import UserNotifications
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,32 +7,8 @@ import UserNotifications
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Firebase'i başlat
-    FirebaseApp.configure()
-
-    // Push Notification izin isteği
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self
-      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-      UNUserNotificationCenter.current().requestAuthorization(
-        options: authOptions,
-        completionHandler: { _, _ in }
-      )
-    }
-    application.registerForRemoteNotifications()
-
-    // FCM delegate
-    Messaging.messaging().delegate = self
-
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  // FCM Token yenilendiğinde
-  override func application(_ application: UIApplication,
-                   didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-    Messaging.messaging().apnsToken = deviceToken
-    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
   }
 
   // URL scheme handler - Facebook ve dış linkleri handle et (Flutter'ın kendi işlemesine bırakılıyor)
@@ -45,12 +18,5 @@ import UserNotifications
     options: [UIApplication.OpenURLOptionsKey : Any] = [:]
   ) -> Bool {
     return super.application(app, open: url, options: options)
-  }
-}
-
-// FCM Delegate
-extension AppDelegate: MessagingDelegate {
-  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-    print("RotaRehber FCM Token: \(fcmToken ?? "nil")")
   }
 }
